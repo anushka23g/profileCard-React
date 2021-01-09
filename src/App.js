4a9ee50f7db18fb96c047f0e1a5566f595d1e2bc
Anushka Gupta <42449841+anushka23g@users.noreply.github.com>
2021-01-09 08:30:35 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.css";
+import Card from "./Card.js"
+export default function App() {
+  return (
+<div className="App">
+    <Card />
+    <Card />
+    <Card />
+    <Card />
+</div>
+  );
+}
