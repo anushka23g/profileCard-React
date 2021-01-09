7108487cb26771adbbc8958d104a4b6e9d3139b9
Anushka Gupta <42449841+anushka23g@users.noreply.github.com>
2021-01-09 09:54:22 +0530

    Revert "Props & Faker.js"

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React from "react";
 import "./styles.css";
-import Card from "./Card.js";
-import Faker from 'faker';
-
+import Card from "./Card.js"
 export default function App() {
   return (
 <div className="App">
-    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SDE-1" image="https://www.w3schools.com/howto/img_avatar.png" />
-    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="Analyst" image="https://www.w3schools.com/howto/img_avatar2.png"/>
-    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SDE-2" image="https://www.w3schools.com/howto/img_avatar.png"/>
-    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SE" image="https://www.w3schools.com/howto/img_avatar.png"/>
-    
+    <Card />
+    <Card />
+    <Card />
+    <Card />
 </div>
   );
 }
