e7695ab3d177aeb425fab0453457467d95dd2495
Anushka Gupta <42449841+anushka23g@users.noreply.github.com>
2021-01-09 08:53:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./styles.css";
-import Card from "./Card.js"
+import Card from "./Card.js";
+import Faker from 'faker';
+
 export default function App() {
   return (
 <div className="App">
-    <Card />
-    <Card />
-    <Card />
-    <Card />
+    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SDE-1" image="https://www.w3schools.com/howto/img_avatar.png" />
+    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="Analyst" image="https://www.w3schools.com/howto/img_avatar2.png"/>
+    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SDE-2" image="https://www.w3schools.com/howto/img_avatar.png"/>
+    <Card name={Faker.name.findName()} email = {Faker.internet.email()} profession="SE" image="https://www.w3schools.com/howto/img_avatar.png"/>
+    
 </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,14 @@
 import React from 'react';
 
-const Card=()=>{
+const Card=(props)=>{
 return (
   <div>
      <div className="card">
-    <img src="https://www.w3schools.com/howto/img_avatar.png" alt="Avatar" style= {{width: 100}}/>
+    <img src={props.image} alt={props.name} style= {{width: 100}}/>
     <div className="container">
-      <h4><b>John Doe</b></h4> 
-      <p>Architect & Engineer</p> 
+      <h4><b>{props.name}</b></h4> 
+      <p>{props.email}</p>
+      <p>{props.profession}</p> 
     </div>
   </div>
   
